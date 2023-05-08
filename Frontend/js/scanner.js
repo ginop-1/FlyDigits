2a4d36d6657c66e3346cf5acd616b9a80f1bd69e
ginop-1 <enrico.covili@fermi.mo.it>
2023-05-08 22:03:47 +0200

    praticamente è finito.
    cercate sempre di stare lontani da js

--- Frontend/js/scanner.js
+++ Frontend/js/scanner.js
@@ -1,103 +1,91 @@
 import QrScanner from "../js/qr-scanner.min.js";
 
-    const video = document.getElementById('qr-video');
-    const videoContainer = document.getElementById('video-container');
-    const camHasCamera = document.getElementById('cam-has-camera');
-    const camList = document.getElementById('cam-list');
-    const camHasFlash = document.getElementById('cam-has-flash');
-    const flashToggle = document.getElementById('flash-toggle');
-    const flashState = document.getElementById('flash-state');
-    const camQrResult = document.getElementById('cam-qr-result');
-    const camQrResultTimestamp = document.getElementById('cam-qr-result-timestamp');
-    const fileSelector = document.getElementById('file-selector');
-    const fileQrResult = document.getElementById('file-qr-result');
+const video = document.getElementById('qr-video');
+const flashToggle = document.getElementById('flash-toggle');
+const camToggle = document.getElementById('camera-toggle');
 
-    function setResult(label, result) {
-        console.log(result.data);
-        label.textContent = result.data;
-        camQrResultTimestamp.textContent = new Date().toString();
-        label.style.color = 'teal';
-        clearTimeout(label.highlightTimeout);
-        label.highlightTimeout = setTimeout(() => label.style.color = 'inherit', 100);
-    }
+let camList = [];
+let currentCam = 0;
+let flashState = false; // false = off, true = on
 
-    // ####### Web Cam Scanning #######
-
-    const scanner = new QrScanner(video, result => setResult(camQrResult, result), {
-        onDecodeError: error => {
-            camQrResult.textContent = error;
-            camQrResult.style.color = 'inherit';
-        },
-        highlightScanRegion: true,
-        highlightCodeOutline: true,
-    });
-
-    const updateFlashAvailability = () => {
-        scanner.hasFlash().then(hasFlash => {
-            camHasFlash.textContent = hasFlash;
-            flashToggle.style.display = hasFlash ? 'inline-block' : 'none';
-        });
-    };
-
-    scanner.start().then(() => {
-        updateFlashAvailability();
-        // List cameras after the scanner started to avoid listCamera's stream and the scanner's stream being requested
-        // at the same time which can result in listCamera's unconstrained stream also being offered to the scanner.
-        // Note that we can also start the scanner after listCameras, we just have it this way around in the demo to
-        // start the scanner earlier.
-        QrScanner.listCameras(true).then(cameras => cameras.forEach(camera => {
-            const option = document.createElement('option');
-            option.value = camera.id;
-            option.text = camera.label;
-            camList.add(option);
-        }));
-    });
-
-    QrScanner.hasCamera().then(hasCamera => camHasCamera.textContent = hasCamera);
-
-    // for debugging
-    window.scanner = scanner;
-
-    document.getElementById('scan-region-highlight-style-select').addEventListener('change', (e) => {
-        videoContainer.className = e.target.value;
-        scanner._updateOverlay(); // reposition the highlight because style 2 sets position: relative
-    });
-
-    document.getElementById('show-scan-region').addEventListener('change', (e) => {
-        const input = e.target;
-        const label = input.parentNode;
-        label.parentNode.insertBefore(scanner.$canvas, label.nextSibling);
-        scanner.$canvas.style.display = input.checked ? 'block' : 'none';
-    });
-
-    document.getElementById('inversion-mode-select').addEventListener('change', event => {
-        scanner.setInversionMode(event.target.value);
-    });
-
-    camList.addEventListener('change', event => {
-        scanner.setCamera(event.target.value).then(updateFlashAvailability);
-    });
-
-    flashToggle.addEventListener('click', () => {
-        scanner.toggleFlash().then(() => flashState.textContent = scanner.isFlashOn() ? 'on' : 'off');
-    });
-
-    document.getElementById('start-button').addEventListener('click', () => {
-        scanner.start();
-    });
-
-    document.getElementById('stop-button').addEventListener('click', () => {
-        scanner.stop();
-    });
-
-    // ####### File Scanning #######
-
-    fileSelector.addEventListener('change', event => {
-        const file = fileSelector.files[0];
-        if (!file) {
-            return;
+function setResult(label, qrResult) {
+    scanner.stop();
+    let server_ip = "flydigitsforfermi.hopto.org:5000" // TODO: move to config file
+    console.log("result: ", qrResult);
+    // make http request to server
+    let url = "https://" + server_ip + "/sensor/?modelname=" + qrResult.data + "&to=2000-1-1 0:0:0"
+    fetch(url, {
+        "headers": {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+            "Origin": "https:\/\/" + server_ip,
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+            "Access-Control-Allow-Credentials": "true"
         }
-        QrScanner.scanImage(file, { returnDetailedScanResult: true })
-            .then(result => setResult(fileQrResult, result))
-            .catch(e => setResult(fileQrResult, { data: e || 'No QR code found.' }));
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === "error") {
+                alert("Model not found");
+                scanner.start();
+                return;
+            }
+            // data.modelName = qrResult.data;
+            localStorage.setItem("result", JSON.stringify(data));
+            window.location.href = "3dmodel.html";
+        })
+        .catch(error => {
+            console.log(error);
+            alert("Error: " + error);
+            scanner.start();
+        });
+}
+
+// ####### Web Cam Scanning #######
+
+const scanner = new QrScanner(video, result => setResult(null, result), {
+    onDecodeError: error => { },
+    highlightScanRegion: true,
+    highlightCodeOutline: true,
+});
+
+const updateFlashAvailability = () => {
+    scanner.hasFlash().then(hasFlash => {
+        flashToggle.style.display = hasFlash ? 'inline-block' : 'inline-block';
     });
+};
+
+scanner.start().then(() => {
+    updateFlashAvailability();
+    // List cameras after the scanner started to avoid listCamera's stream and the scanner's stream being requested
+    // at the same time which can result in listCamera's unconstrained stream also being offered to the scanner.
+    // Note that we can also start the scanner after listCameras, we just have it this way around in the demo to
+    // start the scanner earlier.
+    QrScanner.listCameras(true).then(cameras => cameras.forEach(camera => {
+        camList.push({ label: camera.label, id: camera.id });
+    })).then(QrScanner.setCamera(camList[0].id).then(updateFlashAvailability));
+});
+
+// for debugging
+window.scanner = scanner;
+
+camToggle.addEventListener('click', event => {
+    console.log(camList, currentCam);
+    currentCam = (currentCam + 1) % camList.length;
+    scanner.setCamera(camList[currentCam].id).then(() => {
+        // console.log(`Using camera: ${scanner.getActiveCamera().label}`);
+        updateFlashAvailability();
+    });
+});
+
+flashToggle.addEventListener('click', () => {
+    scanner.toggleFlash()
+    flashState = !flashState;
+    if (flashState === true) {
+        flashToggle.classList.add("active");
+    } else {
+        flashToggle.classList.remove("active");
+    }
+});
--- Frontend/js/zappar.js
+++ Frontend/js/zappar.js
@@ -2,10 +2,97 @@ console.log("Using AFrame", AFRAME.version);
 console.log("Using ZapparAFrame", ZapparAFrame);
 
 window.addEventListener("load", setup);
+const result = JSON.parse(localStorage.getItem("result"));
+result.index = 1;
+
+/* JSON RESULT IS BUILT AS FOLLOWS:
+    [
+        "modelName": "name",
+        [
+            {
+                "sensorName": "name",
+                "type": "type",
+                "unit": "unit", 
+                "measurements": [
+                    [value, timestamp],
+                    [value, timestamp],
+                    ...,
+                ]
+            },
+        ]
+    ] */
+
+function changeSensor(index) {
+    console.log("changeSensor", index);
+    result.index += index;
+    if (result.index <= 0) {
+        result.index = result.length - 1;
+    }
+    if (result.index >= result.length) {
+        result.index = 1;
+    }
+    const myImageGroup = document.getElementById("image-group");
+    myImageGroup.innerHTML = "";
+    generate3dTextList(myImageGroup);
+}
+
+function generate3dTextElement(coordinates, value, id) {
+    return `<a-entity position="${coordinates}" scale="0.25 0.25 1" text-geometry="value: ${value}" material="color: green" id="${id}"></a-entity>`;
+}
+
+function generate3dTextList(myImageGroup) {
+    // add modelname 3d text + arrows
+    myImageGroup.innerHTML += `
+    <a-entity position="-1 0.6 1" scale="0.5 0.5 1" text-geometry="value: ${result[0].modelName}" material="color: red" id="model-name-3dtext"></a-entity>
+    `;
+
+    var index = result.index;
+
+    if (result[index].measurements.length === 0) {
+        return;
+    }
+    try {
+        // current measurement type
+        var type = result[index].type;
+        // current measurement unit
+        var unit = result[index].unit;
+        // last value
+        var lastMeasure = result[index].measurements[result[index].measurements.length - 1][0].toFixed(2);
+        // last pressure
+        var lastTime = result[index].measurements[result[index].measurements.length - 1][1].replace("T", " ").replace("Z", " ");
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    myImageGroup.innerHTML += `
+        ${generate3dTextElement(`-1 0.2`, `${type}: ${lastMeasure} ${unit}`, `${type}-${index}`)}
+        ${generate3dTextElement(`-1 -0.2`, `time: ${lastTime}`, `${type}-${index}-time`)}
+        `;
+}
 
 function setup() {
+    const target = document.getElementById("target");
+    target.setAttribute("src", `../models/${result[0].modelName}.zpt`);
 
     const myImageGroup = document.getElementById("image-group");
+    generate3dTextList(myImageGroup);
+
+    const arrowGroup = document.getElementById("arrow-group");
+    arrowGroup.innerHTML += `
+    <a-entity id="left-arrow" arrow="length: 0.5; direction: -1 0 0" position="-0.75 -1 -1" scale="1 6 1"></a-entity>
+    <a-entity id="right-arrow" arrow="length: 0.5; direction: 1 0 0" position="0.75 -1 -1" scale="1 6 1"></a-entity>
+    `;
+
+    // left arrow\
+    const leftArrow = document.getElementById("left-arrow");
+    leftArrow.addEventListener("click", () => {
+        changeSensor(-1);
+    });
+    // right arrow
+    const rightArrow = document.getElementById("right-arrow");
+    rightArrow.addEventListener("click", () => {
+        changeSensor(1);
+    });
 
     let imageVisible = false;
 
@@ -22,5 +109,12 @@ function setup() {
             if (imageVisible === false) myImageGroup.setAttribute("visible", "false");
         }, 500)
     });
-
 }
+
+// rescan button
+const placementUI = document.getElementById("zappar-placement-ui");
+placementUI.addEventListener("click", function () {
+    window.location.href = "scanner.html";
+});
+
+console.log(JSON.stringify(result));
